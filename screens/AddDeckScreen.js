8d31bdf7c8b4564cbbf0273eb7a4e0dd8e75469e
david-newton <david.newton@xero.com>
2018-11-01 19:10:53 +1300

    Functionality added for adding decks

--- screens/AddDeckScreen.js
+++ screens/AddDeckScreen.js
@@ -1,25 +1,50 @@
 import React from 'react';
 import {Text, TextInput, View, Button} from 'react-native';
+import {AsyncStorage} from 'react-native'
 
 export default class CreateDeckScreen extends React.Component {
 
   constructor(props) {
     super(props)
     this.state = {
-      text: ""
+      title: ""
     }
   }
 
   onPress() {
-    
+    AsyncStorage.getItem('Decks').then(async (value) => {
+      if (value) {
+
+        decks = JSON.parse(value)
+        decks.push({
+          title: this.state.title,
+          cards: []
+        })
+        await AsyncStorage.setItem("Decks", JSON.stringify(decks))
+
+      } else {
+        decks = []
+        decks.push({
+          title: this.state.title,
+          cards: []
+        })
+        await AsyncStorage.setItem("Decks", JSON.stringify(decks))
+      }
+    }).catch(error => {
+      console.log(error)
+    })
   }
 
   render() {
+    var style = {
+      height: 80
+    }
+
     return (
       <View>
         <Text>What is the title of your new deck?</Text>
-        <TextInput onChangeText={(text) => {this.setState({text})}} value={this.state.text}/>
-        <Button title={"Submit"} onPress={this.onPress()}/>
+        <TextInput style={style} onChangeText={(title) => {this.setState({title})}} value={this.state.title}/>
+        <Button title={"Submit"} onPress={() => {this.onPress()}}/>
       </View>
     );
   }
